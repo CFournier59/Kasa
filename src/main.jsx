d193cd0d6e12994fcb1e84115fd476fe8080e0f9
CFournier59 <clementfournier.c@gmail.com>
2025-12-12 11:51:48 +0100

    -création des fichiers jsx relatifs aux pages de l'appli - codage des routes sur src/index.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Home from './pages/Home/index.jsx'
+import About from './pages/About/index.jsx'
+import Housing from './pages/Housing/index.jsx'
+import NotFound from './pages/NotFound/index.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+   <StrictMode>
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/housing/:id" element={<Housing />} />
+            <Route path="*" element={<NotFound />} />
+         </Routes>
+      </BrowserRouter>
+   </StrictMode>
 )
