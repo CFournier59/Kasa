dad09932090919fa421a95d3c1457d7691855a2c
CFournier59 <clementfournier.c@gmail.com>
2025-12-19 10:43:58 +0100

    structure jsx du carousel de la housing page

--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router'
 import housings from '../../data/logements.json'
+import Carousel from '../../components/Carousel'
 
 export default function Housing() {
    const { id } = useParams()
@@ -7,8 +8,7 @@ export default function Housing() {
    return (
       <main>
          <section>
-            {/* <Carousel cover={housing.cover} pictures={housing.pictures} />
-            <Properties /> */}
+            <Carousel cover={housing.cover} pictures={housing.pictures} />
          </section>
       </main>
    )
