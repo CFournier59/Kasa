3acad7629b1446b10065b89e76c99f2d00e20514
CFournier59 <clementfournier.c@gmail.com>
2025-12-22 10:45:06 +0100

    implementation de la structure de housing : - création des composants, TagList, DropDown, et Grade

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -2,18 +2,21 @@ import { useState } from 'react'
 
 export default function Carousel({ cover, pictures }) {
    const [currentSrc, setSrc] = useState(cover)
+
    function handlePrevClick() {
       const currentIndex = pictures.indexOf(currentSrc)
       const prevIndex =
          currentIndex === 0 ? pictures.length - 1 : currentIndex - 1
       setSrc(pictures[prevIndex])
    }
+
    function handleNextClick() {
       const currentIndex = pictures.indexOf(currentSrc)
       const nextIndex =
          currentIndex === pictures.length - 1 ? 0 : currentIndex + 1
       setSrc(pictures[nextIndex])
    }
+
    return (
       <div>
          <div>
