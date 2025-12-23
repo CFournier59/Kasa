e05bbe7b3cf943b1b3c1415120654968dc4c0557
CFournier59 <clementfournier.c@gmail.com>
2025-12-23 16:41:38 +0100

    implementation du style pour le composant Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,11 @@
 import logo from '../../assets/logos/logo-white-38.svg'
+import '../../styles/Footer.scss'
 
 export default function Footer() {
    return (
-      <footer>
-         <img src={logo} alt="Kasa Logo" />
-         <p>© 2020 Kasa. All rights reserved.</p>
+      <footer className="footer">
+         <img className="footer__logo" src={logo} alt="Kasa Logo" />
+         <p className="footer__copyright">© 2020 Kasa. All rights reserved.</p>
       </footer>
    )
 }
