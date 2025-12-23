fcbdd2a226e2005a25e6d65fae878f9107fbc137
CFournier59 <clementfournier.c@gmail.com>
2025-12-23 16:23:03 +0100

    Style du composant Header: - ajout des selecteurs - ajout du style sur le fichier scss associé - style du lien conditionné à la page active

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,30 @@
 import { Link } from 'react-router'
+import { useLocation } from 'react-router'
 import logo from '../../assets/logos/logo-68.svg'
+import '../../styles/Header.scss'
 
 export default function Header() {
+   const location = useLocation()
    return (
-      <div>
-         <img src={logo} alt="Kasa Logo" />
-         <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/about">A Propos</Link>
+      <div className="header">
+         <img className="header__logo" src={logo} alt="Kasa Logo" />
+         <nav className="navbar">
+            <Link
+               className={`navbar__link ${
+                  location.pathname === '/' && 'navbar__link--actual'
+               }`}
+               to="/"
+            >
+               Accueil
+            </Link>
+            <Link
+               className={`navbar__link ${
+                  location.pathname === '/about' && 'navbar__link--actual'
+               }`}
+               to="/about"
+            >
+               A Propos
+            </Link>
          </nav>
       </div>
    )
