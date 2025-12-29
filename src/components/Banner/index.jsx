5019e0ea88364b137edb81b795215a44ab550525
CFournier59 <clementfournier.c@gmail.com>
2025-12-29 10:57:58 +0100

    implementation du style pour le composant Banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,3 +1,15 @@
+import banner1 from '../../assets/banners/banner1.png'
+import banner2 from '../../assets/banners/banner2.png'
+import '../../styles/Banner.scss'
+
 export default function Banner({ whithTitle = false }) {
-   return <div>{whithTitle && <h1>Chez vous, partout et ailleurs</h1>}</div>
+   const bannerLogo = whithTitle ? banner1 : banner2
+
+   return (
+      <div className="banner" style={{ backgroundImage: `url(${bannerLogo})` }}>
+         {whithTitle && (
+            <h1 className="banner__title">Chez vous, partout et ailleurs</h1>
+         )}
+      </div>
+   )
 }
