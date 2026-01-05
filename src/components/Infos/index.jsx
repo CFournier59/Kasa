3896c63e0b446fc21db95ea4bf5df0982a8b03b9
CFournier59 <clementfournier.c@gmail.com>
2026-01-05 16:29:17 +0100

    implementation du style sur le composant Infos : - modification du style sur son sous composant DropDown - implementation du style sur son sous composant Grade

--- src/components/Infos/index.jsx
+++ src/components/Infos/index.jsx
@@ -1,23 +1,26 @@
+import '../../styles/Infos.scss'
 import TagList from '../TagList'
 import Grade from '../Grade'
 import DropDown from '../DropDown'
 
 export default function Infos({ housing }) {
    return (
-      <section>
-         <div>
-            <h1>{housing.title}</h1>
+      <section className="infos">
+         <div className="infos__top-container">
             <div>
+               <h1 className="infos__title">{housing.title}</h1>
+               <p className="infos__location">{housing.location}</p>
+            </div>
+            <div className="infos__host">
                <p>{housing.host.name}</p>
                <img src={housing.host.picture} />
             </div>
          </div>
-         <p>{housing.location}</p>
-         <div>
+         <div className="infos__middle-container">
             <TagList tags={housing.tags} />
             <Grade rating={housing.rating} />
          </div>
-         <div>
+         <div className="infos__bottom-container">
             <DropDown
                title="Description"
                content={<p>{housing.description}</p>}
