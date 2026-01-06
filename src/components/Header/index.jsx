638be3c40b8b12714a34316323f88a73febe481a
CFournier59 <clementfournier.c@gmail.com>
2026-01-06 15:11:35 +0100

    modifications responsives sur le composant Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,28 @@
-import { Link } from 'react-router'
-import { useLocation } from 'react-router'
-import logo from '../../assets/logos/logo-68.svg'
+import { Link, useLocation } from 'react-router'
+import desktopLogo from '../../assets/logos/logo-68.svg'
+import phoneLogo from '../../assets/logos/logo-46.svg'
 import '../../styles/Header.scss'
+import { useEffect, useState } from 'react'
 
 export default function Header() {
    const location = useLocation()
+   const [capSize, setCapSize] = useState(false)
+   const [logo, setLogo] = useState(desktopLogo)
+   useEffect(() => {
+      const handleResize = () => {
+         if (window.innerWidth >= 800) {
+            setCapSize(false)
+            setLogo(desktopLogo)
+         } else {
+            setCapSize(true)
+            setLogo(phoneLogo)
+         }
+      }
+      handleResize()
+      window.addEventListener('resize', handleResize)
+      return () => window.removeEventListener('resize', handleResize)
+   })
+
    return (
       <div className="header">
          <img className="header__logo" src={logo} alt="Kasa Logo" />
@@ -15,7 +33,7 @@ export default function Header() {
                }`}
                to="/"
             >
-               Accueil
+               {capSize ? 'ACCUEIL' : 'Accueil'}
             </Link>
             <Link
                className={`navbar__link ${
@@ -23,7 +41,7 @@ export default function Header() {
                }`}
                to="/about"
             >
-               A Propos
+               {capSize ? 'À PROPOS' : 'À propos'}
             </Link>
          </nav>
       </div>
