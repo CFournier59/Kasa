3299b7dff7cb58da62235ff77c44f3030463092c
CFournier59 <clementfournier.c@gmail.com>
2026-01-08 15:14:23 +0100

    correction du chemin d'import de la data housings

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import Header from './components/Header/index.jsx'
 import Footer from './components/Footer/index.jsx'
 
 async function init() {
-   const response = await fetch('../public/data/logements.json')
+   const response = await fetch('/data/logements.json')
    const text = await response.text()
    return JSON.parse(text)
 }
