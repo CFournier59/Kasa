72023fcf59e1652c64ccd49ac8572d169f7caafe
CFournier59 <clementfournier.c@gmail.com>
2026-01-08 17:13:56 +0100

    implementation de l'animation du composant DropDown

--- src/components/DropDown/index.jsx
+++ src/components/DropDown/index.jsx
@@ -1,30 +1,31 @@
 import { useState } from 'react'
 import '../../styles/DropDown.scss'
-import arrowUp from '../../assets/buttons/arrow-up-24.svg'
-import arrowDown from '../../assets/buttons/arrow-down-24.svg'
-
+import arrow from '../../assets/buttons/arrow-up-24.svg'
 export default function DropDown({ title, content }) {
    const [isOpen, setIsOpen] = useState(false)
-
    const toggleOpen = () => {
       setIsOpen(!isOpen)
    }
-
    return (
       <div className="drop-down">
          <div className="drop-down__top">
             <h2 className="drop-down__title">{title}</h2>
-            <label>
-               <input type="checkbox" className="drop-down__indicator-toggle" />
-               <img
-                  onClick={toggleOpen}
-                  className="drop-down__indicator "
-                  src={arrowUp}
-                  alt="Toggle DropDown"
-               />
-            </label>
+            <img
+               onClick={toggleOpen}
+               className={`drop-down__indicator${
+                  isOpen ? ' drop-down__indicator--open' : ''
+               }`}
+               src={arrow}
+               alt="Toggle DropDown"
+            />
          </div>
-         {isOpen && <div className="drop-down__content">{content}</div>}
+         <div
+            className={`drop-down__content${
+               isOpen ? ' drop-down__content--open' : ''
+            }`}
+         >
+            {content}
+         </div>
       </div>
    )
 }
